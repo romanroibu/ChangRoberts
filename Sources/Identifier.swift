0e46f35540a199c0b4d736a4901606ac40da9814
Roman Roibu <romanroibu@gmail.com>
2017-01-19 16:18:10 +0100

    Extend UUID to conform to UniqueIdentifier protocol

--- Sources/Identifier.swift
+++ Sources/Identifier.swift
@@ -15,3 +15,28 @@ public protocol IdentifiableEntity {
 
     var uid: UID { get }
 }
+
+extension UUID: UniqueIdentifier {
+    public static func < (lhs: UUID, rhs: UUID) -> Bool {
+        return lhs.uuid < rhs.uuid
+    }
+}
+
+public func < (lhs: uuid_t, rhs: uuid_t) -> Bool {
+    return (lhs.0  < rhs.0)
+        || (lhs.1  < rhs.1)
+        || (lhs.2  < rhs.2)
+        || (lhs.3  < rhs.3)
+        || (lhs.4  < rhs.4)
+        || (lhs.5  < rhs.5)
+        || (lhs.6  < rhs.6)
+        || (lhs.7  < rhs.7)
+        || (lhs.8  < rhs.8)
+        || (lhs.9  < rhs.9)
+        || (lhs.10 < rhs.10)
+        || (lhs.11 < rhs.11)
+        || (lhs.12 < rhs.12)
+        || (lhs.13 < rhs.13)
+        || (lhs.14 < rhs.14)
+        || (lhs.15 < rhs.15)
+}
